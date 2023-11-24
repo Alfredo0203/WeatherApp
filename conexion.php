444f8cd7a5294a5cba49f44bcb27f4e3424a9f6d
Diego99ALS <131622431+Diego99ALS@users.noreply.github.com>
2023-11-24 13:33:48 -0600

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-function conectar(){
+//function conectar(){
     $MYSQLHOST = $_ENV["MYSQLHOST"];
     $MYSQLUSER =  $_ENV["MYSQLUSER"];
     $MYSQLPASSWORD =  $_ENV["MYSQLPASSWORD"];
@@ -10,15 +10,7 @@ function conectar(){
     // Crear una conexión con la base de datos
    $con = mysqli_connect("$MYSQLHOST", "$MYSQLUSER", "$MYSQLPASSWORD", "$MYSQL_DATABASE", "$MYSQLPORT");
     
-    // Verificar si hubo errores en la conexión
-    if (mysqli_connect_errno()) {
-        echo "Error de conexión a la base de datos: " . mysqli_connect_error();
-        exit();
-    }
-    
-    // Devolver la conexión establecida
-    return $con;
-}
+
 ?>
 
   
