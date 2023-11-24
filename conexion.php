b525f2f9ee7d4c721f6ee0aa46cbaa76876b53f9
Alfredo0203 <60232665+Alfredo0203@users.noreply.github.com>
2023-11-23 23:03:53 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -4,10 +4,10 @@ function conectar(){
     $MYSQLHOST = $_ENV["MYSQLHOST"];
     $MYSQLUSER =  $_ENV["MYSQLUSER"];
     $MYSQLPASSWORD =  $_ENV["MYSQLPASSWORD"];
-    $MYSQL_DATABASE =  $_ENV["MYSQLDATABASE"];
+    $MYSQL_DATABASE =  $_ENV["MYSQL_DATABASE"];
     
     // Crear una conexión con la base de datos
-    $con = mysqli_connect("$MYSQLHOST'", "$MYSQLUSER", "$MYSQLPASSWORD", "$MYSQLDATABASE");
+    $con = mysqli_connect("$MYSQLHOST'", "$MYSQLUSER", "$MYSQLPASSWORD", "$MYSQL_DATABASE");
     
     // Verificar si hubo errores en la conexión
     if (mysqli_connect_errno()) {
