b62beddcf5cf1250e4c2d445b26150ea356cf3f2
Alfredo0203 <60232665+Alfredo0203@users.noreply.github.com>
2023-11-23 22:56:11 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
 
 function conectar(){
-    $MYSQLHOST = "viaduct.proxy.rlwy.net";
-    $MYSQLUSER = "root";
-    $MYSQLPASSWORD = "dcHDd1c-f3546e-HC2AB3Abgf4A3fd-5";
-    $MYSQLDATABASE = "railway";
+    $MYSQLHOST = $_ENV["MYSQLHOST"];
+    $MYSQLUSER =  $_ENV["MYSQLUSER"];
+    $MYSQLPASSWORD =  $_ENV["MYSQLPASSWORD"];
+    $MYSQLDATABASE =  $_ENV["MYSQLDATABASE"];
     
     // Crear una conexión con la base de datos
-    $con = mysqli_connect($MYSQLHOST, $MYSQLUSER, $MYSQLPASSWORD, $MYSQLDATABASE);
+    $con = mysqli_connect("$MYSQLHOST'", "$MYSQLUSER", "$MYSQLPASSWORD", "$MYSQLDATABASE");
     
     // Verificar si hubo errores en la conexión
     if (mysqli_connect_errno()) {
